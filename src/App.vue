7d97a2b075bd8dd0f7ed2e6e2028125d15a0324c
luis fernando cifuentes <luis.fernando.cifuentes@correounivalle.edu.co>
2019-07-17 17:47:21 -0500

    upload files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="hero is-white is-gradient is-bold">
+      <div class="hero-body">
+        <h1 class="title">
+          <span class="has-text-success" >
+            R&M
+          </span>
+          <span class="subtitle">
+            Personajes
+          </span>
+        </h1>
+        <button class="button is-success is-rounded" v-on:click="fetch">
+          Consultar
+        </button>
+      </div>
+    </div>
+    <div class="container">
+      <div class="columns is-desktop is-mobile is-tablet is-multiline is-centered">
+        <character :character="character" v-for="character in characters" v-bind:key="character.id" ></character>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+  import axios from "axios";
+  import Character from "./components/Character";
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Character
+  },
+  data(){
+    return {
+      characters:[]
+    }
+  },
+  created(){
+    this.fetch();
+  },
+  methods:{
+    fetch:function () {
+      axios.get("https://rickandmortyapi.com/api/character")
+        .then(res=>{
+          this.characters = res.data.results;
+        })
+
+        .catch(err=>{
+          console.log(err);
+        })
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
